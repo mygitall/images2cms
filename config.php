43dc853f645fcf273c669f8cbeb616702ac5cc73
mygitall <zhangweiweiguge@gmail.com>
2026-05-27 11:41:51 +0800

    update config

--- config.php
+++ config.php
@@ -6,19 +6,23 @@ return array (
     'default' => 
     array (
       'api_key' => 'sk-etNyRftUVqza8jbAgSiQ19zMkKkVTrqpJtKocVEdcBTBfzAB',
-      'base_url' => 'https://api.tokln.com',
+      'base_url' => 'https://api.tokln.com/',
     ),
   ),
-  'save_dir' => '/Applications/MAMP/htdocs/images/uploads',
   'features' => 
   array (
-    'show_folder_card' => false,
+    'show_folder_card' => true,
     'show_presets' => true,
-    'disable_register' => false,
+    'disable_register' => true,
     'daily_reg_max' => '2',
     'register_block_msg' => '暂时停止注册',
-    'site_announcement' => true,
+    'site_announcement' => false,
     'site_announcement_text' => '欢迎访问！叔叔ss',
+    'log_retention_days' => '0',
+    'new_user_free_count' => '0',
+    'global_total_max' => '0',
+    'global_daily_max' => '0',
+    'banned_ips_list' => '0',
   ),
   'installed' => true,
 );
